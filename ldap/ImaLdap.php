45307118c7d576bde1e4a0cb8bb657feacd69d9a
Dmitry Karpov <d.karpov@cherinfo.ru>
2016-04-22 12:43:26 +0300

    Ldap module moved to repo

---
+++ ldap/ImaLdap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace demetrio77\imautils\ldap;
+
+use yii\base\Object;
+use demetrio77\imautils\ldap\LdapImaTrait;
+
+class ImaLdap extends Object
+{
+	use LdapImaTrait;
+	
+	public function init()
+	{
+		parent::init();
+		$this->connect();
+	}
+}
